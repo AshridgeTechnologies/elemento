7b7b9ded76872a42a8bf6109aa7abc95d1d2bdc9
paulb <paulb@ashridgetech.com>
2022-08-12 10:30:29 +0100

    WIP - Runtime library - Non-working attempt to create library with Parcel

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,7 +1,16 @@
-import * as components from './components'
+import * as allComponents from './components'
 import {globalFunctions} from './globalFunctions'
 import appFunctions from './appFunctions'
 import {useGetObjectState, useObjectState} from './appData'
 import {asArray, codeGenerationError, parentPath} from './runtimeFunctions'
+import AppRunner from './AppRunner'
 
-export {components, globalFunctions, appFunctions, useObjectState, useGetObjectState, codeGenerationError, asArray, parentPath}
+export const components = {
+    App: allComponents.App,
+    Page: allComponents.Page,
+    TextElement: allComponents.TextElement,
+}
+export {globalFunctions, appFunctions, useObjectState, useGetObjectState, codeGenerationError, asArray, parentPath, AppRunner}
+
+export {default as React} from 'react'
+export {default as ReactDOM} from 'react-dom/client'
