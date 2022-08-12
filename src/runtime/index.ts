6633d4aaabe0aa306e6b0668e6e5326f9438d14d
paulb <paulb@ashridgetech.com>
2022-08-12 11:57:07 +0100

    WIP - Runtime library - Example spike running with library built with esbuild

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,16 +1,10 @@
-import * as allComponents from './components'
+import * as components from './components'
 import {globalFunctions} from './globalFunctions'
-import appFunctions from './appFunctions'
-import {useGetObjectState, useObjectState} from './appData'
-import {asArray, codeGenerationError, parentPath} from './runtimeFunctions'
-import AppRunner from './AppRunner'
-
-export const components = {
-    App: allComponents.App,
-    Page: allComponents.Page,
-    TextElement: allComponents.TextElement,
-}
-export {globalFunctions, appFunctions, useObjectState, useGetObjectState, codeGenerationError, asArray, parentPath, AppRunner}
+export {components, globalFunctions}
 
 export {default as React} from 'react'
 export {default as ReactDOM} from 'react-dom/client'
+export {default as appFunctions} from './appFunctions'
+export {useGetObjectState, useObjectState} from './appData'
+export {asArray, codeGenerationError, parentPath} from './runtimeFunctions'
+export {default as AppRunner} from './AppRunner'
