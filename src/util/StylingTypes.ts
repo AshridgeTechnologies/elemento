77b86d7f14b7d43857fa955894fbf1713864befc
paulb <paulb@ashridgetech.com>
2024-04-10 12:11:45 +0100

    Add flex to styles properties

--- src/util/StylingTypes.ts
+++ src/util/StylingTypes.ts
@@ -19,6 +19,7 @@ export const commonStylingPropTypes: {[p: string]: PropertyType} = {
     color: 'string',
     cursor: ['auto', 'default', 'none', 'context-menu', 'help', 'pointer', 'progress', 'wait', 'cell', 'crosshair', 'text', 'vertical-text', 'alias', 'copy', 'move', 'no-drop', 'not-allowed', 'grab', 'grabbing', 'e-resize', 'n-resize', 'ne-resize', 'nw-resize', 's-resize', 'se-resize', 'sw-resize', 'w-resize', 'ew-resize', 'ns-resize', 'nesw-resize', 'nwse-resize', 'col-resize', 'row-resize', 'all-scroll', 'zoom-in', 'zoom-out'],
     display: ['inline', 'block', 'inline-block', 'none', 'revert'],
+    flex: 'string',
     font: 'string',
     fontFamily: 'string',
     fontSize: 'string',
