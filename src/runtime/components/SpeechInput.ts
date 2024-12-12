0d850b4be7d875f20ec98a3ea74f1f31ee6da4ff
paulb <paulb@ashridgetech.com>
2024-12-12 10:08:43 +0000

    Bug fix - Speech Input can be restarted while stopping

--- src/runtime/components/SpeechInput.ts
+++ src/runtime/components/SpeechInput.ts
@@ -8,8 +8,9 @@ type Properties = {path: string}
 type ExternalProperties = {language?: string, expectedPhrases?: string[]}
 type StateProperties = {speechRecognition: any, recording: boolean, value: string, confidence: number | null}
 
+type RecognitionStatus = 'ready' | 'started' | 'stopping' | 'stopping_startRequested'
 type SpeechRecognitionExt = SpeechRecognition
-    & {onaudiostart: any, onaudioend: any, onstart: any, onend: any, onsoundstart: any, onsoundend: any, onspeechstart: any, onspeechend: any}
+    & {onaudiostart: any, onaudioend: any, onstart: any, onend: any, onsoundstart: any, onsoundend: any, onspeechstart: any, onspeechend: any, status: RecognitionStatus}
 
 export default function SpeechInput({path}: Properties) {
     return createElement('div', {id: path})
@@ -45,7 +46,7 @@ export class SpeechInputState extends BaseComponentState<ExternalProperties, Sta
         recognition.interimResults = false
         recognition.maxAlternatives = 3
 
-        const logEvent = (event: SpeechRecognitionEvent) => console.log('SpeechRecognition', event.type)
+        const logEvent = (event: SpeechRecognitionEvent) => console.log('SpeechRecognition', event.type, recognition.status)
         recognition.onresult = (event: SpeechRecognitionEvent) => {
             const firstResult = event.results[0][0]
             console.log('SpeechRecognition', '- Result:', firstResult)
@@ -67,38 +68,64 @@ export class SpeechInputState extends BaseComponentState<ExternalProperties, Sta
 
         recognition.onaudiostart = logEvent
         recognition.onaudioend = logEvent
-        recognition.onend = logEvent
+        recognition.onend = (e: any) => {
+            logEvent(e)
+            if (recognition.status === 'stopping_startRequested') {
+                this.doStart()
+            } else {
+                recognition.status = 'ready'
+            }
+        }
         recognition.onsoundstart = logEvent
         recognition.onsoundend = logEvent
         recognition.onspeechstart = logEvent
         recognition.onstart = logEvent
+        recognition.status = 'ready'
         return recognition
     }
 
     Start() {
-        if (this.recording) {
+        const recognition = this.speechRecognition
+        if (recognition.status === 'started' || recognition.status === 'stopping_startRequested') {
             return
         }
+        if (recognition.status === 'stopping') {
+            recognition.status = 'stopping_startRequested'
+            return
+        }
+        this.doStart()
+    }
+
+    Stop() {
+        this.requestStop()
+        this.updateState({recording: false})
+    }
+
+    Reset() {
+        this.requestStop()
+        this.updateState({value: undefined, recording: false, confidence: null})
+    }
+
+    private requestStop() {
+        const recognition = this.speechRecognition
+        if (recognition.status === 'started') {
+            recognition.stop()
+            recognition.status = 'stopping'
+        }
+    }
+
+    private doStart() {
         const recognition = this.speechRecognition
         if (this.props.expectedPhrases) {
-            const grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + this.props.expectedPhrases.join(' | ') +';'
+            const grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + this.props.expectedPhrases.join(' | ') + ';'
             const speechRecognitionList = new SpeechGrammarList()
             speechRecognitionList.addFromString(grammar, 1)
             recognition.grammars = speechRecognitionList
         }
         recognition.lang = this.props.language
         recognition.start()
+        recognition.status = 'started'
         this.updateState({value: undefined, recording: true, confidence: null})
-    }
-
-    Stop() {
-        this.speechRecognition.stop()
-        this.updateState({recording: false})
-    }
-
-    Reset() {
-        this.speechRecognition.stop()
-        this.updateState({value: undefined, recording: false, confidence: null})
     }
 }
 
