7e44ed800f65cb31408e7cdd6f9a50f644136fce
paulb <paulb@ashridgetech.com>
2025-04-15 16:49:13 +0100

    WIP - Preview from wrangler dev server - Use popstate events instead of buggy Navigation API polyfill

--- src/runtime/navigationHelpers.ts
+++ src/runtime/navigationHelpers.ts
@@ -1,4 +1,3 @@
-import "@virtualstate/navigation/polyfill"
 import eventObservable from '../util/eventObservable'
 import Observable from 'zen-observable'
 
@@ -11,7 +10,15 @@ export const convertSearch = (search: string) => {
 }
 
 let urlChangeObservable: Observable<any>
-export const getUrlChangeObservable = () => urlChangeObservable ??= eventObservable((window as any).navigation, 'navigate', (event: any) => event.destination.url)
+const navigationApiAvailable = (window as any).navigation !== undefined
+const createUrlChangeObservable = () => {
+    if (navigationApiAvailable) {
+        return eventObservable((window as any).navigation, 'navigate', (event: any) => event.destination.url)
+    } else {
+        return eventObservable(window, 'popstate', (_event: any) => window.location.href)
+    }
+}
+export const getUrlChangeObservable = () => urlChangeObservable ??= createUrlChangeObservable()
 
 export const onUrlChange = (callback: CallbackFn) => {
     return getUrlChangeObservable().subscribe(callback)
@@ -27,5 +34,8 @@ export const goForward = () => {
 
 export const pushUrl = (newUrl: string) => {
     window.history.pushState(null, '', newUrl)
+    if (!navigationApiAvailable) {
+        window.dispatchEvent(new Event('popstate'))
+    }
 }
 
