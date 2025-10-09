58eed80755036dfa148572da5e85a46f6e874949
paulb <paulb@ashridgetech.com>
2025-10-09 09:22:00 +0100

    WIP - test state in children can be accessed by parent

--- tests/runtime/appDataStateInChildren.test.ts
+++ tests/runtime/appDataStateInChildren.test.ts
@@ -1,14 +1,14 @@
-import { afterEach, beforeEach, afterAll, beforeAll, describe, expect, it, vi, test } from "vitest"  
 /**
  * @vitest-environment jsdom
  */
-
+import {expect, test} from 'vitest'
 import {createElement} from 'react'
 import {testContainer} from '../testutil/rtlHelpers'
-import {setObject, useObject} from '../../src/runtime/appStateHooks'
-import {BaseComponentState} from '../../src/runtime/components/ComponentState'
+import {use$state} from '../../src/runtime/state/appStateHooks'
+import {BaseComponentState} from '../../src/runtime/components/ComponentState2'
 
 import {StoreProvider} from '../../src/runner/StoreContext'
+import {wait} from '../testutil/testHelpers'
 
 let stateInParent: any = null
 let stateInChild: any = null
@@ -19,19 +19,20 @@ class Thing extends BaseComponentState<{a: number, b: string}>{
 }
 
 function Child(props: object) {
-    stateInChild = useObject('app.parent')
+    stateInChild = use$state('app.parent.child', Thing, {a: 10, b: 'Bee'})
     return createElement('div', props)
 }
 
 function Parent(props: object) {
-    stateInParent = setObject('app.parent', new Thing({a: 10, b: 'Bee'}))
+    stateInParent = use$state('app.parent.child')
     return createElement(Child, props)
 }
 
-test('Child can get state set by parent with the same class', () => {
+test('Parent can get state set by child with the same class', async () => {
     testContainer(createElement(StoreProvider, null, createElement(Parent)))
+    await wait()
     expect(stateInChild).toBeInstanceOf(Thing)
     expect(stateInChild.a).toBe(10)
     expect(stateInChild.b).toBe('Bee')
-    expect(stateInChild).toBe(stateInParent)
+    expect(stateInParent._raw).toBe(stateInChild._raw)
 })
